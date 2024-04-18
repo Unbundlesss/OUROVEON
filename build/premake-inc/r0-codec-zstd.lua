3e69d3ef650a3265f562b0de41b1e748780a1892
Harry Denholm <harry.denholm@gmail.com>
2024-04-18 22:34:35 +0100

    zstd 1.5.6 added

---
+++ build/premake-inc/r0-codec-zstd.lua
@@ -0,0 +1,37 @@
+
+LibRoot.Zstd = SrcDir() .. "r0.codec/zstd"
+
+-- ==============================================================================
+ModuleRefInclude["zstd"] = function()
+    includedirs
+    {
+        LibRoot.Zstd .. "/lib",
+    }
+    defines 
+    { 
+        "ZSTD_MULTITHREAD=0",
+        "ZSTD_DLL_EXPORT=0",
+        "ZSTD_LEGACY_SUPPORT=0"
+    }
+end
+
+-- ==============================================================================
+project "r0-zstd"
+    kind "StaticLib"
+    language "C"
+
+    SetDefaultBuildConfiguration()
+    SetDefaultOutputDirectories("r0")
+
+    ModuleRefInclude["zstd"]()
+
+    files
+    {
+        LibRoot.Zstd .. "/**.c",
+        LibRoot.Zstd .. "/**.h",
+    }
+
+ModuleRefLink["zstd"] = function()
+    links { "r0-zstd" }
+end
+
