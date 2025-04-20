29bde134c5abc8cac196711d79de33800ecb37da
Harry Denholm <harry.denholm@gmail.com>
2025-04-20 17:58:57 +0100

    changes after re-testing on Mint 22 w. Clang-19 and sndio

--- build/premake-linux.sh
+++ build/premake-linux.sh
@@ -1,9 +1,9 @@
 
-# sudo apt install clang-17 lld-17
+# sudo apt install clang-19 lld-19
 # sudo apt install libc++-dev
 # sudo apt install libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev libgl-dev xorg-dev
 # sudo apt install libopus-dev libasound2-dev libjack-jackd2-dev jackd2
-# sudo apt install libssl-dev libsodium-dev libflac-dev libflac++-dev
+# sudo apt install libssl-dev libsodium-dev libflac-dev libflac++-dev libsndio-dev
 
 
 # Fedora 39
@@ -22,15 +22,15 @@
 ./premake-bin/linux/premake5 --file=premake.lua gmake2
 
 
-export CC=/usr/bin/clang-17
-export CPP=/usr/bin/clang-cpp-17
-export CXX=/usr/bin/clang++-17
+export CC=/usr/bin/clang-19
+export CPP=/usr/bin/clang-cpp-19
+export CXX=/usr/bin/clang++-19
 
 #export CXXFLAGS=" -fsanitize=address -fsanitize=undefined -fno-omit-frame-pointer -g "
-#export LDFLAGS="-v -fsanitize=address -fsanitize=undefined -fuse-ld=lld-17 "
+#export LDFLAGS="-v -fsanitize=address -fsanitize=undefined -fuse-ld=lld-19 "
 
 export CXXFLAGS=" "
-export LDFLAGS="-v -fuse-ld=lld-17 "
+export LDFLAGS="-v -fuse-ld=lld-19 "
 
 
 cd _gen
