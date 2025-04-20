5becec1353d21ddcadf133951e8399f419366808
Harry Denholm <harry.denholm@gmail.com>
2025-04-20 15:44:00 +0100

    fix the source patching of the ASIO SDK to make it actually work

--- build/premake-inc/asiosdk.lua
+++ build/premake-inc/asiosdk.lua
@@ -50,10 +50,22 @@ function checkASIOSDKDownload( buildRootDir )
                 -- dumb patching of Win32 calls to force MBCS versions as this file can't compile under Unicode
                 asioListContents = string.gsub(asioListContents, 'CharLowerBuff%(',      '::CharLowerBuffA(')
                 asioListContents = string.gsub(asioListContents, 'RegOpenKey%(',         '::RegOpenKeyA(' )
+                asioListContents = string.gsub(asioListContents, 'RegOpenKeyEx%(',       '::RegOpenKeyExA(' )
                 asioListContents = string.gsub(asioListContents, 'RegEnumKey%(',         '::RegEnumKeyA(' )
                 asioListContents = string.gsub(asioListContents, 'RegQueryValueEx%(',    '::RegQueryValueExA(' )
                 asioListContents = string.gsub(asioListContents, ',%(LPTSTR%)',          ',(LPSTR)' )
+                asioListContents = string.gsub(asioListContents, ',%(LPCTSTR%)',         ',(LPCSTR)' )
                 asioListContents = string.gsub(asioListContents, ',strlen',              ', (DWORD)strlen' )
+
+                -- the original code uses OpenFile (16-bit compat function) to test if an ASIO driver exists on-disk
+                -- .. and doesn't bother to close the handle after!
+                -- so we patch it out to use GetFileAttributesA instead, which is neater and doesn't leak handles
+                asioListContents = string.gsub(asioListContents,
+                    'hfile = OpenFile%(dllpath,&ofs,OF_EXIST%);',
+                    'DWORD dwAttrib = GetFileAttributesA( dllpath ); hfile = 0;' )
+                asioListContents = string.gsub(asioListContents,
+                    'if %(hfile%) rc = 0;',
+                    'if (dwAttrib != INVALID_FILE_ATTRIBUTES) rc = 0;' )
 
                 local ok, err = os.rename( asioListFileToPatch, asioListFileToPatch .. "_old" )
                 if not ok then
